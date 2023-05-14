15e9253d920b48169cee67f8cc8e9b5ca2dfd76b
julimer228 <julia-merta@wp.pl>
2023-05-14 15:22:35 +0200

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,67 @@ import Register from "./pages/register/Register";
 import Login from "./pages/login/Login";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
+import "./app.scss"
+
+import Navbar from "./components/navbar/Navbar";
+import Leftbar from "./components/leftbar/Leftbar";
+import Rightbar from "./components/rightbar/Rightbar";
+
 
 import{
   createBrowserRouter,
   RouterProvider,
   Route,
+  Outlet,
+  Navigate,
 } from "react-router-dom";
+
+
 
 function App() {
 
 
+  const currentUser = true;
 
   const Layout = () =>{
     return(
-      <div>
-        
+      <div className="page">
+        <Navbar/>
+        <div className="center-part">
+          <Leftbar/>
+          <Outlet/>
+          <Rightbar/>
+        </div>
       </div>
     )
   }
 
+  const ProtectedRoute = ({children}) =>{
+
+    if(!currentUser){
+      return <Navigate to={"/login"}></Navigate>
+    }
+    return children;
+  }
+
 
   const router = createBrowserRouter([
+
+    {
+      path:"/",
+      element:<ProtectedRoute>
+        <Layout/>
+        </ProtectedRoute>,
+      children:[{
+        path: "/",
+        element:<Home/>,
+      },
+      {
+        path:"/profile/:id",
+        element:<Profile/>
+      }
+  ]
+    },
     {
       path:"/login",
       element: <Login/>,
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,41 @@
 import "./navbar.scss"
+import CottageIcon from '@mui/icons-material/Cottage';
+import Brightness6Icon from '@mui/icons-material/Brightness6';
+import GridViewIcon from '@mui/icons-material/GridView';
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+import EmailIcon from '@mui/icons-material/Email';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import Person2Icon from '@mui/icons-material/Person2';
+import { Link } from "react-router-dom";
 
 const Navbar = () =>{
     return(
-        <div className="navbar">Navbar</div>
+        <div className="navbar">
+            <div className="left">
+                <Link to="/" style={{textDecoration:"none"}}>
+                <span>Medits</span>
+                </Link>
+                <CottageIcon className="icon"/>
+                <Brightness6Icon className="icon"/>
+                <GridViewIcon className="icon"/>
+                <div className="search">
+                    <ManageSearchIcon className="icon"/>
+                    <input type="text" placeholder="Search"></input>
+                </div>
+            </div>
+
+            <div className="right">
+                <EmailIcon className="icon"/>
+                <NotificationsIcon className="icon"/>
+                <Person2Icon className="icon"/>
+                <div className="user">
+                    <img src="src\images\profile-women.jpg"/>
+                    <span>
+                        Ann Mika
+                    </span>
+                </div>
+            </div>
+        </div>
     );
 };
 
