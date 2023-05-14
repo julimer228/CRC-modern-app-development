87905e04d4028894748ad442b63ca7ad737776dd
julimer228 <julia-merta@wp.pl>
2023-05-14 11:20:54 +0200

    styled login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -7,13 +7,7 @@ const Login = () =>{
                 <div className="left">
                     <h1>Hello World!</h1>
                     <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                     when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                     It has survived not only five centuries, but also the leap into electronic typesetting, 
-                     remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-                     sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
-                     Aldus PageMaker including versions of Lorem Ipsum.
+                    Join our study community. Let's begin the journey!
                     </p>
                     <span>Do you have an account?</span>
                      <button>Register</button>
