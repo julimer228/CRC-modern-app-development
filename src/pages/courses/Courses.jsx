26bcd926274cfd11c653de0ce6fb86f602e5e9e5
julimer228 <julia-merta@wp.pl>
2023-05-16 07:20:55 +0200

    added left form

--- src/pages/courses/Courses.jsx
+++ src/pages/courses/Courses.jsx
@@ -9,6 +9,24 @@ const Courses = () =>{
                 <span>Create course</span>
             </div>
             <div className="courses">
+                <div className="left">
+                <h1>
+                    Login
+                </h1>
+                    <form>
+                        <input type ="text" placeholder="Course name"/>
+                        <input type ="text" placeholder="Date"/>
+                        <input type ="text" placeholder="Duration"/>
+                        <input type ="text" placeholder="Price"/>
+                        <input type ="text" placeholder="Teacher"/>
+                        <input type ="text" placeholder="Language"/>
+                        <button>Create Course</button>
+                    </form>
+                </div>
+
+                <div className="right">
+                    
+                </div>
 
 
             </div>      
